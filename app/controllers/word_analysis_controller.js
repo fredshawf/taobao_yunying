0031fa2be659c0bda5609813525350f5838aa891
fredxiaoF <gift5168xiaofeng@qq.com>
2018-11-26 15:36:40 +0800

    完成需求1

--- app/controllers/word_analysis_controller.js
+++ app/controllers/word_analysis_controller.js
@@ -1,15 +1,42 @@
+nodejieba = require("nodejieba");
+
 module.exports = class WordAnalysisController {
   
   async index() {
+    let article = this.params['article'] || '';
+    let words = this.params['words'] || [];
     
-    await this.render('word_analysis/index', {a: 'index action'});
-    
+    await this.render('word_analysis/index', {article: article, words: words});
   }
   
   
   
   async create() {
-    await this.render('word_analysis/index', {a: 'index action'});
+    let article = this.params['article'];
+    
+    // 移除所有标点
+    let r = '[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~，。、！￥……（）—？《》「」·；：“”‘’ ]+';
+    let reg = new RegExp(r, 'g');
+    article = article.replace(reg, '');
+    
+    // 中文分词
+    let words = nodejieba.cut(article);
+    
+    // 统计词频
+    words.unshift(new Map());
+    let words_group = words.reduce((r, e) => {
+      r.set(e, (r.get(e) ? r.get(e) + 1 : 1)); return r;
+    })
+    
+    // 排序
+    let words_list = Array.from(words_group.entries())
+    words_list = words_list.sort((x, y) => {
+      return y[1] - x[1];
+    })
+    
+    this.params['words'] = words_list
+    
+    await this.index();
   }
   
 }
