6a4c8a1e26c105f14286c7ee25d88261488599e9
fredxiaoF <gift5168xiaofeng@qq.com>
2018-12-14 18:12:41 +0800

    update

--- app/controllers/top20_words/upload_controller.js
+++ app/controllers/top20_words/upload_controller.js
@@ -1,10 +1,20 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const XLSX = require('xlsx')
+
 module.exports = class UploadController{
   
+  async new() {
+    
+    await this.render("top20_words/upload")
+  }
   
   async create() {
-    
-    
-    
+    let file_path = this.params.tags.path;
+    // let xlsx_buffer = await fs.readFileAsync(file_path);
+    let workbook = XLSX.readFile(file_path)
+    // console.log(xlsx_buffer.byteLength)
+    this.ctx.body = '123'
   }
   
   
